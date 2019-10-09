a00ede6325ec1c33fe7ee15119ca5b145b415428
AndrewLihasenko <lihasenko@google.com>
2019-10-09 21:10:07 +0300

    refactoring and add some testcase

--- tests/tests.py
+++ tests/tests.py
@@ -3,50 +3,74 @@ import unittest
 from homework import Rectangle
 
 
-rect_valid_value = Rectangle(width=25, height=25)
+rectangle_value = Rectangle(width=35, height=50)
+square_value = Rectangle(width=35, height=35)
 
 
 class Test(unittest.TestCase):
 
     def test_get_rectangle_perimeter(self):
-        actual_result = rect_valid_value.get_rectangle_perimeter()
-        expected_result = (rect_valid_value.width +
-                           rect_valid_value.height) * 2
+        actual_result = rectangle_value.get_rectangle_perimeter()
+        expected_result = (rectangle_value.width +
+                           rectangle_value.height) * 2
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_square_perimeter(self):
+        actual_result = square_value.get_rectangle_perimeter()
+        expected_result = (square_value.width +
+                           square_value.height) * 2
         self.assertEqual(actual_result, expected_result)
 
     def test_get_rectangle_square(self):
-        actual_result = rect_valid_value.get_rectangle_square()
-        expected_result = rect_valid_value.width * rect_valid_value.height
+        actual_result = rectangle_value.get_rectangle_square()
+        expected_result = rectangle_value.width * rectangle_value.height
         self.assertEqual(actual_result, expected_result)
 
-    def test_get_sum_of_corners(self):
-        corners = 3
-        actual_result = rect_valid_value.get_sum_of_corners(corners)
-        if corners < 5:
-            expected_result = corners * 90
-            self.assertEqual(actual_result, expected_result)
-        else:
-            self.assertRaises(ValueError, actual_result)
+    def test_get_rectangle_square_for_square(self):
+        actual_result = square_value.get_rectangle_square()
+        expected_result = square_value.width * square_value.height
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_sum_of_corners_valid_value(self):
+        for corners in range(1, 4):
+            with self.subTest(i=corners):
+                actual_result = rectangle_value.get_sum_of_corners(corners)
+                expected_result = corners * 90
+                self.assertEqual(actual_result, expected_result)
+
+    def test_get_sum_of_corners_raises_error(self):
+        with self.assertRaises(ValueError):
+            corners = 5
+            rectangle_value.get_sum_of_corners(corners)
 
     def test_get_rectangle_diagonal(self):
-        actual_result = rect_valid_value.get_rectangle_diagonal()
-        expected_result = math.sqrt(math.pow(rect_valid_value.height, 2) +
-                                    math.pow(rect_valid_value.width, 2))
+        actual_result = rectangle_value.get_rectangle_diagonal()
+        expected_result = math.sqrt(math.pow(rectangle_value.height, 2) +
+                                    math.pow(rectangle_value.width, 2))
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_square_diagonal(self):
+        actual_result = square_value.get_rectangle_diagonal()
+        expected_result = math.sqrt(math.pow(square_value.height, 2) +
+                                    math.pow(square_value.width, 2))
         self.assertEqual(actual_result, expected_result)
 
     def test_get_radius_of_circumscribed_circle(self):
-        actual_result = rect_valid_value.get_radius_of_circumscribed_circle()
-        expected_result = (math.sqrt(math.pow(rect_valid_value.height, 2) +
-                                    math.pow(rect_valid_value.width, 2))) / 2
+        actual_result = rectangle_value.get_radius_of_circumscribed_circle()
+        expected_result = (math.sqrt(math.pow(rectangle_value.height, 2) +
+                                     math.pow(rectangle_value.width, 2))) / 2
         self.assertEqual(actual_result, expected_result)
 
-    def test_get_radius_of_inscribed_circle(self):
-        actual_result = rect_valid_value.get_radius_of_inscribed_circle()
-        if rect_valid_value.height == rect_valid_value.width:
-            expected_result = rect_valid_value.height / 2
-            self.assertEqual(actual_result, expected_result)
-        else:
-            self.assertRaises(ValueError, actual_result)
+    def test_get_radius_of_inscribed_circle_valid_value(self):
+        actual_result = square_value.get_radius_of_inscribed_circle()
+        diagonal = math.sqrt(math.pow(square_value.height, 2) +
+                             math.pow(square_value.width, 2))
+        expected_result = diagonal / 2 * math.sqrt(2)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_radius_of_inscribed_circle_raises_error(self):
+        with self.assertRaises(ValueError):
+            rectangle_value.get_radius_of_inscribed_circle()
 
 
 if __name__ == "__main__":
