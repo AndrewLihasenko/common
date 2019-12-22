00e3c065ccac3930ac55be5e9f3c0c59670ffb08
AndrewLihasenko <lihasenko@google.com>
2019-12-23 01:22:15 +0200

    fixed bug when dont create tables

--- flask_ORM/config.py
+++ flask_ORM/config.py
@@ -1,27 +1,24 @@
 import os
 
 
-PG_USER = "flask_orm"
-PG_PASSWORD = "123"
-PG_HOST = "localhost"
-PG_PORT = 5432
-DB_NAME = "flask_orm"
-SQLALCHEMY_DATABASE_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 class Config:
-    SECRET_KEY = "key"
+    PG_USER = "flask_orm"
+    PG_PASSWORD = "123"
+    PG_HOST = "localhost"
+    PG_PORT = 5432
+    DB_NAME = "flask_orm"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
 
 
 class TestConfig(Config):
-    SECRET_KEY = "test_key"
+    TEST_VALUE = "TEST_CONFIG_VALUE"
     DEBUG = True
 
 
 class ProdConfig(Config):
-    SECRET_KEY = "prod_key"
+    TEST_VALUE = "PROD_CONFIG_VALUE"
     DEBUG = False
 
 
